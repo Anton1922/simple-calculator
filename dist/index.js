7a674eeebde84b08e639ad1bdf35762703544e97
Anton Fedorov <anton.pzkt@gmail.com>
2020-05-03 18:49:29 +0300

    distributional files

--- dist/index.js
+++ dist/index.js
@@ -45,6 +45,16 @@ function calcResult() {
   mathOperation = '';
   return result;
 }
+
+function checkInputValue() {
+  if (calculatorInput.value === '' || !isFinite(calculatorInput.value)) {
+    alert('Enter a number!');
+    calculatorInput.value = '';
+    return true;
+  }
+
+  return false;
+}
 /*** Add Event Listener for Keyboard ***/
 
 
@@ -56,9 +66,8 @@ calculatorKeyboard.addEventListener('click', function (event) {
     console.log('target.innerHTML', target.innerHTML);
 
     if (result != '') {
-      calculatorInput.value = target.innerHTML; //console.log('result', result);
-
-      result = ''; //console.log('result', result);
+      calculatorInput.value = target.innerHTML;
+      result = '';
     } else {
       calculatorInput.value += target.innerHTML;
     }
@@ -67,43 +76,36 @@ calculatorKeyboard.addEventListener('click', function (event) {
 
 
   if (target.classList.contains('calculator__button--math-sqrt')) {
-    if (calculatorInput.value === '') {
-      alert('Enter a number!');
+    if (!checkInputValue()) {
+      calcMathSqrt();
     }
-
-    calcMathSqrt();
   }
   /*** Calculate Math Operations ***/
 
 
   if (target.classList.contains('calculator__button--math-operation')) {
-    if (calculatorInput.value === '') {
-      alert('Enter a number!');
+    if (!checkInputValue()) {
+      numberFirst = calculatorInput.value;
+      calculatorInput.value = '';
+      mathOperation = target.innerHTML;
     }
-
-    numberFirst = calculatorInput.value; //console.log('numberFirst', numberFirst);
-
-    calculatorInput.value = '';
-    mathOperation = target.innerHTML; //console.log('mathOperation', mathOperation);
   }
   /*** Calculate ResultT ***/
 
 
   if (target.classList.contains('calculator__button--equal')) {
-    //console.log('result', result);
-    numberSecond = calculatorInput.value; //console.log('numberSecond', numberSecond);
-
-    calcResult();
+    if (!checkInputValue()) {
+      numberSecond = calculatorInput.value;
+      calcResult();
+    }
   }
   /*** Change Number Sign ***/
 
 
   if (target.classList.contains('calculator__button--plus-minus')) {
-    if (calculatorInput.value === '') {
-      alert('Enter a number!');
+    if (!checkInputValue()) {
+      calculatorInput.value = String(-1 * +calculatorInput.value);
     }
-
-    calculatorInput.value = String(-1 * +calculatorInput.value);
   }
   /*** Clear Input by AC-button ***/
 
